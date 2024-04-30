e43d21e5babe003bb14f82691c19ea225b56a028
sedia <dia.serignfalou@gmail.com>
2024-04-30 14:03:53 +0000

    vol reservation

---
+++ reservation/armsApp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from armsApp import models
+
+# Register your models here.
+admin.site.register(models.Airlines)
+admin.site.register(models.Airport)
+admin.site.register(models.Flights)
+admin.site.register(models.Reservation)
